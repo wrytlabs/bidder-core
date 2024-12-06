f3f2e9747c6ae189222452f22fd6cd32304d8e11
samclassix <samclassix@proton.me>
2024-12-06 17:38:44 +0100

    feat: close funding, adjust refRatio and total values

--- contracts/BidderManagerV1.sol
+++ contracts/BidderManagerV1.sol
@@ -25,25 +25,20 @@ contract BidderManagerV1 is ERC721 {
 
 	uint256 public tokenCnt;
 	uint256 public totalFunds;
+	uint256 public totalRewards;
 	uint256 public refRatio = 1 ether;
 
-	struct Challenge {
-		address challenger; // the address from which the challenge was initiated
-		uint64 start; // the start of the challenge
-		IPositionV1 position; // the position that was challenged
-		uint256 size; // how much collateral the challenger provided
+	struct Fund {
+		uint256 amount;
+		uint256 refRatio;
 	}
 
-	struct Position {
-		uint256 funds;
-		uint256 ref;
-	}
-
-	mapping(uint256 tokenId => Position) public funding;
+	mapping(uint256 tokenId => Fund) public funds;
 
 	// ---------------------------------------------------------------------------------------
 
-	event Created(address indexed owner, uint256 tokenId, uint256 amount, uint256 ref);
+	event Create(address indexed owner, uint256 tokenId, uint256 amount, uint256 refRatio);
+	event Close(address indexed owner, uint256 tokenId, uint256 funds, uint256 payout);
 	event Execute(
 		address indexed executor,
 		uint256 challengeIndex,
@@ -57,6 +52,7 @@ contract BidderManagerV1 is ERC721 {
 	// ---------------------------------------------------------------------------------------
 
 	error NoChange();
+	error NoFunds();
 	error NoLoss(uint256 zchfBefore, uint256 zchfAfter);
 	error WrongInputToken(address input, address needed);
 	error WrongOutputToken(address output, address needed);
@@ -95,23 +91,45 @@ contract BidderManagerV1 is ERC721 {
 
 		zchf.transferFrom(to, address(this), amount);
 		totalFunds += amount;
-		funding[tokenCnt].funds = amount;
-		funding[tokenCnt].ref = refRatio;
+		funds[tokenCnt].amount = amount;
+		funds[tokenCnt].refRatio = refRatio;
 
 		_mint(to, tokenCnt);
 
-		emit Created(to, tokenCnt, amount, refRatio);
+		emit Create(to, tokenCnt, amount, refRatio);
 		return tokenCnt;
+	}
+
+	// ---------------------------------------------------------------------------------------
+
+	function close(uint256 tokenId) public {
+		// check ownership
+		address owner = ownerOf(tokenId);
+		if (owner != msg.sender) revert ERC721IncorrectOwner(msg.sender, tokenId, owner);
+
+		Fund memory user = funds[tokenId];
+		uint256 diff = refRatio - user.refRatio;
+		uint256 payout = user.amount * (1 + diff / 1 ether);
+
+		// clean up
+		totalFunds -= user.amount;
+		_burn(tokenId);
+		delete funds[tokenId];
+
+		// pay out
+		zchf.transfer(msg.sender, payout);
+
+		emit Close(msg.sender, tokenId, user.amount, payout);
 	}
 
 	// ---------------------------------------------------------------------------------------
 
 	function quoteAuction(
 		uint256 index
-	) public returns (IERC20 collateralToken, uint256 collateralAmount, uint256 zchfNeeded) {
+	) public view returns (address collateralToken, uint256 collateralAmount, uint256 zchfNeeded) {
 		// Get challenge info
 		(, , IPositionV1 position, uint256 size) = hub.challenges(index);
-		IERC20 collateral = IERC20(address(position.collateral()));
+		address collateral = address(position.collateral());
 		if (size == 0) return (collateral, 0, 0);
 
 		// Get auction price
@@ -154,8 +172,8 @@ contract BidderManagerV1 is ERC721 {
 
 	// ---------------------------------------------------------------------------------------
 
-	function execute(uint256 index, address[] memory tokens, uint24[] memory fees, uint256 amountIn) public {
-		(IERC20 coll, uint256 size, uint256 toBid) = quoteAuction(index);
+	function execute(uint256 index, address[] memory tokens, uint24[] memory fees) public {
+		(address coll, uint256 size, uint256 toBid) = quoteAuction(index);
 
 		// check token path entry and outcome
 		if (tokens[0] != address(coll)) revert WrongInputToken(tokens[0], address(coll));
@@ -164,27 +182,28 @@ contract BidderManagerV1 is ERC721 {
 
 		// check zchf balance before
 		uint256 zchfBefore = zchf.balanceOf(address(this));
+		if (zchfBefore == 0) revert NoFunds();
 
 		// take bid
 		hub.bid(uint32(index), size, false);
 
-		// approve to router
-		coll.approve(address(swapRouter), coll.balanceOf(address(this)));
-
 		// swap
-		uint256 swapped = executeSwap(tokens, fees, amountIn, toBid);
+		uint256 swapped = executeSwap(tokens, fees, size, toBid);
 
 		// play fair
-		zchf.transfer(msg.sender, (swapped * fee) / 1_000_000);
+		zchf.transfer(msg.sender, (swapped * fee) / 1_000_000); // @dev: pay out executor fee PPM
 
 		// check zchf balance afterwards
 		uint256 zchfAfter = zchf.balanceOf(address(this));
 		if (zchfAfter < zchfBefore) revert NoLoss(zchfBefore, zchfAfter);
 
-		uint256 profit = zchfAfter - zchfBefore; // overflow checked before
+		// profit, overflow checked before
+		uint256 profit = zchfAfter - zchfBefore;
+		refRatio += (profit * 1 ether) / totalFunds;
+		totalRewards += profit;
 
 		// emit
-		emit Execute(msg.sender, index, address(coll), size, toBid, swapped, profit);
+		emit Execute(msg.sender, index, coll, size, toBid, swapped, profit);
 	}
 
 	function executeSwap(
@@ -193,14 +212,21 @@ contract BidderManagerV1 is ERC721 {
 		uint256 amountIn,
 		uint256 amountOutMinimum
 	) internal returns (uint256) {
+		// approve to router
+		IERC20(tokens[0]).approve(address(swapRouter), IERC20(tokens[0]).balanceOf(address(this)));
+
+		// setup swap
 		ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
 			path: encodePath(tokens, fees),
 			recipient: address(this),
 			deadline: block.timestamp,
 			amountIn: amountIn,
-			amountOutMinimum: amountOutMinimum
+			amountOutMinimum: amountOutMinimum // @dev: first check, to ensure to cover bid amount
 		});
 
+		// execute and return
 		return swapRouter.exactInput(params);
 	}
+
+	// ---------------------------------------------------------------------------------------
 }
